cce03e1471a3e7ff15af620a73b047e502deea30
BriefbombenBela <132848785+BriefbombenBela@users.noreply.github.com>
2025-05-12 12:41:13 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,13 @@
+export const metadata = {
+  title: "ETF Rechner",
+  description: "Simuliere ETF-Sparpläne",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
 
